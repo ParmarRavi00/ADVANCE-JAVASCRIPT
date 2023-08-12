0a9ddcfb3f033da5e432492289ab96ca35f1a43d
Parmar Raviraj Rajendrasinh <parmarraviraj00@gmail.com>
2023-08-12 11:34:51 +0530

    .

---
+++ LAB__WORK/file01.js
@@ -0,0 +1,13 @@
+export let abc = "Ravirajsinh";
+
+export function user() {
+    console.log("Hello Everyone");
+}
+
+export class test {
+    constructor(x) {
+        console.log("Constructor Method in" + " " + x);
+    }
+}
+
+// export { abc, user, test };
